c9bab7ca5f58ad3061646a57692215693ee3d811
bashUCLA <116211659+bashUCLA@users.noreply.github.com>
2022-12-02 00:15:27 -0800

    Update arduino_fuzzy_test.ino
    Updated to integrate with data from the sensors.

--- MPPT Tracking/MATLAB_Fuzzy_Test/arduino_fuzzy_test.ino
+++ MPPT Tracking/MATLAB_Fuzzy_Test/arduino_fuzzy_test.ino
@@ -4,6 +4,20 @@ union BtoF
   float fval;  
 } u;
 
+float SENSE_VOLT = 0;
+float SENSE_CURR = 0;
+float SENSE_POWER = 0;
+
+float ERROR = 0;
+float ERROR_PREV = 0;
+
+float DELTA_VOLT = 0;
+float DELTA_POWER = 0;
+float DELTA_ERROR = 0;
+
+
+float VOLT_PREV = 0;
+
 const int buffer_size = 16;
 byte buf[buffer_size];
 
@@ -17,11 +31,12 @@ void setup()
 
 void loop()
 {
+  Calculate_Fuzzy_Inputs();
   if (Serial.available()>0)
   {
     myVal = readFromMatlab();
     delay(0.1);
-    writeToMatlab(2, 1);
+    writeToMatlab(ERROR, DELTA_ERROR);
   }
 }
 
@@ -44,4 +59,38 @@ void writeToMatlab (float error, float change)
   Serial.write(b2, 4);
   Serial.write(13); // "\r"
   Serial.write(10); // "\n"
+}
+
+void SenseOutput_Current()
+{
+  SENSE_CURR = analogRead(A0);
+  SENSE_CURR = (SENSE_CURR / ADC_RES)*(ADC_REF);
+  SENSE_CURR = SENSE_CURR / FB_RATIO_CURR;
+}
+
+void SenseOutput_Voltage()
+{
+  VOLT_PREV = SENSE_VOLT;
+  SENSE_VOLT = analogRead(A3);
+  SENSE_VOLT = (SENSE_VOLT / ADC_RES)*(ADC_REF);
+  SENSE_VOLT = (SENSE_VOLT / FB_RATIO_VOLT);
+}
+
+void Calculate_Fuzzy_Inputs()
+{
+  POWER_PREV = SENSE_POWER;
+
+  SenseOutput_Voltage();
+  SenseOutput_Current();
+
+  SENSE_POWER = SENSE_VOLT * SENSE_CURR;
+
+  DELTA_VOLT = SENSE_VOLT - VOLT_PREV;
+  DELTA_POWER = SENSE_POWER - POWER_PREV;
+
+  ERROR_PREV = ERROR;
+
+  ERROR = (DELTA_POWER / DELTA_VOLT);
+
+  DELTA_ERROR = ERROR - ERROR_PREV;
 }
