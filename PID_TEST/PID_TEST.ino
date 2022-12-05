37ae40fc32c96958ff36854fc56011350f0b185e
Selasi Etchey <selasietchey@ucla.edu>
2022-12-04 16:34:53 -0800

    Test

--- PID_TEST/PID_TEST.ino
+++ PID_TEST/PID_TEST.ino
@@ -33,9 +33,14 @@ float DELTA_VOLT = 0;
 
 float DC = 0;
 
+float Imp = 0;
+float Vmp = 0;
+float Pmp = 0;
+
 
 double Setpoint, Input, Output;
-double Kp=13, Ki=100, Kd=0;
+double Kp=13, Ki=200, Kd=0;
+//double Kp=100, Ki=100, Kd=0;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 mbed::PwmOut pwmPin(digitalPinToPinName(PWM_PIN));
 
@@ -53,81 +58,72 @@ void setup()
   pinMode(A3, INPUT);
   digitalWrite(DIRECTION_PIN, HIGH);
   pwmPin.period( 1.0 / PWM_FREQUENCY );
-  pwmPin.write( 0.5 );
+  pwmPin.write( 0.1 );
   //pinMode(A0, OUTPUT);
   Serial.begin(115200);
-  //myPID.SetMode(AUTOMATIC);
+  myPID.SetMode(AUTOMATIC);
   Temp_Setup();
 
-  Setpoint = 1;
-
+  Setpoint = 3;
 }
 
-void loop() {
+void loop() 
+{
   SenseOutput_Voltage();
   SenseOutput_Current();
   Process_Temp_Pixels_Average();
-
+  
   sense[0] = (uint8_t)SENSE_VOLT;
   sense[1] = (uint8_t)SENSE_CURR;
   sense[2] = (uint8_t)SENSE_TEMP;
 
- /* 
+
+  /*
   Serial.print("Sense Voltage: ");
   Serial.println(sense_v_loop);
   Serial.print("Sense Current: ");
   Serial.println(sense_i_loop);
   Serial.print("Sense Temp: ");
   Serial.println(SENSE_TEMP);
+  Serial.print("Duty Cycle: ");
+  Serial.println(DC);
   */
+
+  
+  //UpdatePot_DC();
   
 
   Serial.write((uint8_t*)sense,sizeof(sense));
   Serial.write("\r\n");
 
-  //while(Serial.available() == 0);
-/*
+
+  while(Serial.available() == 0);
+
   if (Serial.available() > 0) 
   {
-    irr_est = (10* Serial.read());
-    //Imp = calculate_Imp(irr_est, SENSE_TEMP);
-    //Vmp = calculate_Vmp(SENSE_TEMP);
-    //Pmp = Vmp * Imp;
-    //digitalWrite(LED_BUILTIN, HIGH);
-    //delay(500);
-    //digitalWrite(LED_BUILTIN, LOW);
-    //delay(500);
+    irr_est = (8* Serial.read());
+    Imp = calculate_Imp(irr_est, SENSE_TEMP);
+    Vmp = calculate_Vmp(SENSE_TEMP);
+    Pmp = Vmp * Imp;
+    Setpoint = Vmp;
+    UpdatePID();
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
   }
-  */
-  
-  UpdatePot_DC();
-  //Serial.println(DC);
-  
-  /*
-  //UpdatePID();
-  Serial.print("Sense Voltage: ");
-  Serial.println(sense_v_loop);
-  Serial.print("Duty Cycle: ");
-  Serial.println(DC);
-  Serial.print("Output Current: ");
-  Serial.println(SENSE_CURR);
-  //Serial.print("Average Temperature: ");
-  //Serial.println(SENSE_TEMP);
-  delay(500);
 
-  */
-  delay(300);
+  delay(100);
 }
 
 void Pert_Obs()
 {
   SenseOutput_Voltage();
   SenseOutput_Current();
+  POWER_PREV = SENSE_POWER;
 
-  SENSE_POWER = SENSE_VOLT * SENSE_CURR;
+  SENSE_POWER = sense_v_loop * sense_i_loop;
   DELTA_POWER = SENSE_POWER - POWER_PREV;
 
-  DELTA_VOLT = SENSE_VOLT - VOLT_PREV;
+  DELTA_VOLT = sense_v_loop - VOLT_PREV;
   if(DELTA_POWER !=0 || DELTA_VOLT != 0)
   {
     if(DELTA_POWER > 0)
@@ -190,6 +186,8 @@ void Process_Temp_Pixels_Average()
 
 void SenseOutput_Voltage()
 {
+  VOLT_PREV = SENSE_VOLT;
+
   SENSE_VOLT = analogRead(A3);
   sense_v_loop = SENSE_VOLT;
   SENSE_VOLT = SENSE_VOLT / 4;
@@ -220,20 +218,31 @@ void UpdatePID()
 {
   //Input = SENSE_VOLT;
   Input = sense_v_loop;
+  //Input = sense_i_loop;
   myPID.Compute();
   DC = (Output / 255);
   pwmPin.write(DC);
 }
 
+
 float calculate_Imp(double irr_est, double Temp)
 {
+  float Imp_ref = 0.35;
+  float Gref = 1000;
+  float alpha_T = 0.046;
+  float Tref = 25;
+
   float imp = Imp_ref * (irr_est/Gref)*(1+(alpha_T*(Temp-Tref)));
   return imp;
 }
 
 float calculate_Vmp(double Temp)
 {
+  float beta_T = -0.0118;
+  float Tref = 25;
+  float Vmp_ref = 6;
   float vmp = Vmp_ref + (beta_T*(Temp-Tref));
   return vmp;
 }
+
 
